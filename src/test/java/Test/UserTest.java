65a34449df01b6cd8ce968a320957bb3f9b05c67
ICEY-Pro <1539901457@qq.com>
2022-06-22 16:38:17 +0800

    mybatis入门程序

--- src/test/java/Test/UserTest.java
+++ src/test/java/Test/UserTest.java
@@ -13,23 +13,27 @@ import java.io.Reader;
 public class UserTest {
     @Test
     public void userFindByIdTest(){
+        //1.获取mybatis核心配置文件
         String resources = "mybatis-config.xml";
         //创建流
         Reader reader = null;
         try{
-            //读取mybatis-config.xml文件内容到reader对象中
+            //读取mybatis-config.xml核心配置文件内容到reader对象中
             reader = Resources.getResourceAsReader(resources);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //2.创建SqlSessionFactory工厂对象
         //初始化MyBatis数据库，创建SqlSessionFactory类的实例
         SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
-        //创建SqlSession实例
+        //3.创建SqlSession实例
         SqlSession session = sqlMapper.openSession();
+        //4.执行sql语句，sql语句的唯一标识：namespace.id
         //传入参数查询，返回结果
         User user = session.selectOne("findById",1);
         //输出结果
         System.out.println(user.getUname());
+        //5.释放资源
         //关闭session
         session.close();
     }
